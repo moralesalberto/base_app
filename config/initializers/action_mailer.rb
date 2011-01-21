b48b07c3460334fe6e7b6c7bb33b38bae8848638
Alberto Morales <alberto@moralitos.com>
2011-01-21 09:18:45 -0500

    updating the license and removing extra items not needed for a base app

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,10 +1,1 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.opnet.com", 
-  :port => 25, 
-  :domain => "opnet.com",
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-
-}
 
